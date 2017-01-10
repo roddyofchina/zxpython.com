a00d9e22e2f85822ef3c5f243c7dbcd07947bb20
roddyofchina <923401910@qq.com>
2017-01-10 11:34:39 +0800

    添加首页

---
+++ webapp/urls.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+__author__ = 'Luodi'
+
+
+from django.conf.urls import url
+from webapp import views
+
+urlpatterns = [
+
+]
+
+
+
+
--- zxpython/settings.py
+++ zxpython/settings.py
@@ -105,18 +105,23 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
